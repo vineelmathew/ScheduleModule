7410748568d63f189b6e0949229bfb911c30d14b
Vineel Mathew <vineelrudrapati9@gmail.com>
2020-09-28 16:07:38 +0530

    project created

---
+++ src/main/java/com/trainee/schedulemodule/SchedulemoduleApplication.java
@@ -0,0 +1,13 @@
+package com.trainee.schedulemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulemoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulemoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainee/schedulemodule/SchedulemoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainee.schedulemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulemoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
