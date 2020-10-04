b56718995811071acb1086c5d9bef7eb2dbc50d3
vineelmathew <vineelrudrapati9@gmail.com>
2020-10-04 07:42:30 +0530

    microservices added

--- src/main/java/com/trainee/schedulemodule/SchedulemoduleApplication.java
+++ src/main/java/com/trainee/schedulemodule/SchedulemoduleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SchedulemoduleApplication {
 
